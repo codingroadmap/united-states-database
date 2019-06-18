117bbda469d29081fb8c8dc155996c563a4f739b
Josh Levy <joshaaronlevy@gmail.com>
2019-06-17 20:21:34 -0600

    Initial pre-knex setup

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+const states = require('./routes/states');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/states', states);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err.stack : {}
+  });
+});
+
+module.exports = app;
